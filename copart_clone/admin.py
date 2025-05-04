675052a07b7a746599f3261b5aa84a697ea89022
revisaosegura <segurarevisao@gmail.com>
2025-05-04 00:20:57 -0300

    Add django-extensions

--- copart_clone/admin.py
+++ copart_clone/admin.py
@@ -3,8 +3,7 @@ from django.contrib import admin, messages
 from django.urls import path
 from django.shortcuts import redirect
 from django.utils.html import format_html
-
-from .models import ScraperPlaceholder  # Modelo simples para exibir o botão
+from .models import ScraperPlaceholder, FerramentaScraper  # Modelo simples para exibir o botão
 
 class ScraperAdmin(admin.ModelAdmin):
     list_display = ("name",)
@@ -31,3 +30,22 @@ class ScraperAdmin(admin.ModelAdmin):
         return redirect('..')
 
 admin.site.register(ScraperPlaceholder, ScraperAdmin)
+
+class FerramentaScraperAdmin(admin.ModelAdmin):
+    change_list_template = "admin/ferramenta_scraper_changelist.html"
+
+    def get_urls(self):
+        urls = super().get_urls()
+        my_urls = [
+            path('executar-scraper/', self.executar_scraper, name='executar-scraper'),
+        ]
+        return my_urls + urls
+
+    def executar_scraper(self, request):
+        # 👉 Aqui é onde roda sua lógica do scraper
+        # Exemplo simples:
+        messages.add_message(request, messages.INFO, "Scraper foi executado!")
+        # ✅ Redireciona de volta para admin
+        return HttpResponseRedirect("../")
+
+admin.site.register(FerramentaScraper, FerramentaScraperAdmin)
