307df35daef349ff6fb81aadd9307c7e131316f2
revisaosegura <segurarevisao@gmail.com>
2025-05-03 22:28:00 -0300

    Add

--- copart_clone/admin.py
+++ copart_clone/admin.py
@@ -1,34 +1,21 @@
 from django.contrib import admin, messages
-from .admin_tools import FerramentasCopartAdmin
+from django.urls import path
+from django.http import HttpResponseRedirect
 from .models import FerramentaCopart
 from auto_scraper.scraper import start_scraping
 
 @admin.register(FerramentaCopart)
 class FerramentaCopartAdmin(admin.ModelAdmin):
-    def has_add_permission(self, request):
-        return False
-
-    def has_change_permission(self, request, obj=None):
-        return False
-
-    def changelist_view(self, request, extra_context=None):
-        if 'run' in request.GET:
-            start_scraping()
-            self.message_user(request, "✅ Scraping executado com sucesso.", level=messages.SUCCESS)
-        return super().changelist_view(request, extra_context)
+    change_list_template = "admin/ferramentas_copart.html"
 
     def get_urls(self):
-        from django.urls import path
         urls = super().get_urls()
         custom_urls = [
-            path('run/', self.admin_site.admin_view(self.changelist_view), name='ferramenta_copart_run'),
+            path('atualizar-copart/', self.admin_site.admin_view(self.atualizar_copart), name='ferramenta_copart_run'),
         ]
         return custom_urls + urls
-    
-@admin.register(object)
-class DummyAdmin(FerramentasCopartAdmin):
-    pass
 
-@admin.register(FerramentaCopart)
-class FerramentaCopartAdminView(FerramentasCopartAdmin):
-    pass
+    def atualizar_copart(self, request):
+        start_scraping()
+        self.message_user(request, "✅ Scraping executado com sucesso.", level=messages.SUCCESS)
+        return HttpResponseRedirect("../")
