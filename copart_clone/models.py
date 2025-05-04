1ba93d4a258de022590774362c8f858ad9f392b0
revisaosegura <segurarevisao@gmail.com>
2025-05-04 00:48:43 -0300

    Add django-extensions

--- copart_clone/models.py
+++ copart_clone/models.py
@@ -1,8 +1,18 @@
 
 from django.db import models
 
-class AtualizarTemplates(models.Model):
+class ScraperPlaceholder(models.Model):
+    name = models.CharField(max_length=100, default="Executar Scraper")
+
     class Meta:
-        verbose_name = 'Executar Atualização'
-        verbose_name_plural = '⚙️ Atualizar Templates (Executar Scraper)'
-        managed = False  # ✅ NÃO cria tabela no banco
+        verbose_name = "Executar Scraper"
+        verbose_name_plural = "Executar Scraper"
+
+    def __str__(self):
+        return self.name
+
+class FerramentaScraper(models.Model):
+    class Meta:
+        verbose_name = 'Atualizar Templates'
+        verbose_name_plural = 'Ferramenta: Atualizar Templates'
+        managed = False  # NÃO cria tabela
