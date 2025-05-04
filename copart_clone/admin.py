38cc071da5aea6fc37e69a8b4514d0239aae536c
revisaosegura <segurarevisao@gmail.com>
2025-05-03 23:52:20 -0300

    Fix URL resolver in admin index template

--- copart_clone/admin.py
+++ copart_clone/admin.py
@@ -1,23 +1,14 @@
-
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponseRedirect
+from django.contrib.auth.models import User
 from django.contrib import messages
-from .models import FerramentasCopart
-from .scraper import rodar_scraper  # Ajuste conforme o nome real do módulo
+from auto_scraper.scraper import start_scraping
 
-@admin.register(FerramentasCopart)
-class FerramentasCopartAdmin(admin.ModelAdmin):
-    change_list_template = "admin/index.html"
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    actions = ['atualizar_copart']
 
-    def get_urls(self):
-        urls = super().get_urls()
-        custom_urls = [
-            path('run/', self.admin_site.admin_view(self.run_scraper), name='run-scraper'),
-        ]
-        return custom_urls + urls
+    def atualizar_copart(self, request, queryset):
+        start_scraping()
+        self.message_user(request, "✅ O scraping foi executado com sucesso.", level=messages.SUCCESS)
 
-    def run_scraper(self, request):
-        rodar_scraper()
-        self.message_user(request, "Scraper executado com sucesso!", messages.SUCCESS)
-        return HttpResponseRedirect("../")
+    atualizar_copart.short_description = "🔄 Atualizar Copart Agora"
