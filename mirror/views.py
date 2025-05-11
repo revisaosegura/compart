06ff6371bbff033389ab6e202cb01c8c0cde6de7
revisaosegura <segurarevisao@gmail.com>
2025-05-10 23:35:21 -0300

    Fix deployment files

--- mirror/views.py
+++ mirror/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, redirect
 
 def home_redirect(request):
     """Redireciona para a página inicial do clone"""
-    return redirect('/static/index.html')  # ou outro HTML espelhado
+    return redirect('/templates/index.html')  # ou outro HTML espelhado
 
 def admin_redirect(request):
     """Redireciona tentativas de acessar /admin"""
