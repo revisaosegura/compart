c171d1d5b9e64c17f4c7828ce3c0dbe2c5f8afa5
revisaosegura <segurarevisao@gmail.com>
2025-05-26 23:41:59 -0300

    Fixando index para Django e corrigindo assets

--- copart_clone/settings.py
+++ copart_clone/settings.py
@@ -46,7 +46,7 @@ ROOT_URLCONF = 'copart_clone.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'copart_clone/static'],  # ou [] se não tiver pasta de templates personalizada
+        'DIRS': [BASE_DIR / 'copart_clone/templates/copart'],  # ou [] se não tiver pasta de templates personalizada
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
